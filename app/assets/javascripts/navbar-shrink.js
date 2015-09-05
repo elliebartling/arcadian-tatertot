8e4bf63a053f979a8dc539b7c49da8f54bb1ba52
elliebartling <ellenbartling@gmail.com>
2015-09-05 13:42:32 -0500

    Installed devise, bootstrap

---
+++ app/assets/javascripts/navbar-shrink.js
@@ -0,0 +1,10 @@
+// NAVBAR SHRINK
+// =============
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module Treebook
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    
+    # Instructions from Devise  
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
